84171b744f9b4083d7a1fcbe3dbd6391e38656ec
bruno-lima1504 <102754701+bruno-lima1504@users.noreply.github.com>
2022-11-16 13:16:05 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { EntryDataComponent } from './components/entry-data.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, EntryDataComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component ({
+  seletor: 'menu-component',
+  template: '<h2>Bruno</h2>',
+  styles: ['h2{color:blue;']
+
+})
+
+export class MenuComponent {
+
+}
